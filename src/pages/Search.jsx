341a9eb7deda1a2ba8736942d02a058e8e3b0dfd
Rafael Godoy <rafa.qfg@gmail.com>
2022-03-07 19:15:18 -0300

    requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
 import Header from '../components/Header';
 
+const MIN_CHAR = 2;
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      search: '',
+      searchSubmitButtonDisabled: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({
+      search: target.value,
+    }, () => {
+      this.setState({
+        searchSubmitButtonDisabled: false,
+      });
+      if (target.value.length < MIN_CHAR) {
+        this.setState({
+          searchSubmitButtonDisabled: true,
+        });
+      }
+    });
+  }
+
+  handleClick = () => {
+    console.log('click');
+  }
+
   render() {
+    const { search, searchSubmitButtonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
@@ -14,13 +44,15 @@ class Search extends React.Component {
               id="searchArtist"
               placeholder="Insira banda ou artista"
               data-testid="search-artist-input"
+              value={ search }
+              onChange={ this.handleChange }
             />
           </label>
           <button
             data-testid="search-artist-button"
-            disabled="disabled"
+            disabled={ searchSubmitButtonDisabled }
             type="submit"
-            // onClick={ console.log('oi') }
+            onClick={ this.handleClick }
           >
             Pesquisar
           </button>
